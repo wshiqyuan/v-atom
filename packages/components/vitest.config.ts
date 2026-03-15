6fc2b402285b7d6b75745e8089aec9ec7c52c810
wshiqyuan <3453177021@qq.com>
2026-03-15 22:37:36 +0800

    refactor(components): 添加类型断言确保插件兼容性

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -1,11 +1,12 @@
+import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig ({
   plugins: [
-    vue(),
-    vueJsx(),
+    vue() as PluginOption,
+    vueJsx() as PluginOption,
   ],
   test: {
     environment: 'jsdom',
